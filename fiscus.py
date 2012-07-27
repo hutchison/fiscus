d5d1ff195758e1deb64daa2f950299d16d1d1c89
Martin Darmüntzel <git@trivialanalog.de>
2012-07-27 21:58:18 +0200

    Erste funktionierende Version. Kann Buchungen in einer Datenbank speichern.

--- fiscus.py
+++ fiscus.py
@@ -1,29 +1,111 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
 import argparse
 import subprocess
 
+import csv
+from datetime import date
+from decimal import Decimal
 
-def check_latin1_file_encoding(filename):
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Numeric, Date
+
+from sqlalchemy.orm import sessionmaker
+
+DBNAME = 'buchungen.db'
+
+Base = declarative_base()
+
+
+class Buchung(Base):
+    __tablename__ = 'buchungen'
+
+    datum = Column(Date, primary_key=True)
+    typ = Column(String)
+    betrag = Column(Numeric)
+    gegenseite = Column(String)
+    verwendungszweck = Column(String, primary_key=True)
+    kontonummer = Column(Integer)
+    blz = Column(Integer)
+
+    def __repr__(self):
+        return "<B('%s', '%s', '%s', '%s', '%s', '%s', '%s')>" %\
+                (self.datum, self.typ, self.betrag, self.gegenseite,
+                        self.verwendungszweck, self.kontonummer,
+                        self.blz)
+
+
+def ist_datei_latin1(filename):
     ftyp = str(subprocess.check_output(['file', '-b', filename]))
     return 'ISO-8859' in ftyp
 
 
-def convert_latin1file_to_utf8(filename):
+def latin1_nach_utf8(filename):
     name, ext = os.path.splitext(filename)
-    filename = name + '-utf8.' + ext
-    with open(filename, 'w') as f:
-        ret = subprocess.call(['iconv', '-f LATIN1 -t UTF-8', filename],
-                stdout=f)
+    new_filename = name + '-utf8' + ext
+    with open(new_filename, 'w') as f:
+        ret = subprocess.call(['iconv', '-f LATIN1', '-t UTF-8',
+            filename], stdout=f)
         assert ret == 0
+        return new_filename
+
+
+def buchungen_aus_datei(filename):
+    buchungen = []
+    reader = csv.DictReader(open(filename, 'r'), delimiter=';')
+    for row in reader:
+        e = Buchung()
+        e.typ = row['Buchungstext']
+        e.verwendungszweck = row['Verwendungszweck']
+        e.gegenseite = row['Begünstigter/Zahlungspflichtiger']
+        if row['Kontonummer']:
+            e.kontonummer = int(row['Kontonummer'])
+        e.blz = int(row['BLZ'])
+
+        betrag = row['Betrag'].replace(',', '.')
+        e.betrag = Decimal(betrag)
+
+        dvs = list(map(int, row['Valutadatum'].split('.')))
+        valutadatum = date(2000 + dvs[2], dvs[1], dvs[0])
+        e.datum = valutadatum
+
+        buchungen.append(e)
+    return buchungen
+
+
+def buchungen_in_db_speichern(buchungen):
+    engine = create_engine('sqlite:///' + DBNAME)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    for b in buchungen:
+        unwanted = s.query(Buchung).filter_by(
+                datum=b.datum,
+                verwendungszweck=b.verwendungszweck).first()
+        if unwanted is None:
+            s.add(b)
+    s.commit()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
-    pass
+
+    parser.add_argument('command')
+    parser.add_argument('args', nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+
+    if args.command == 'add':
+        buchungsdatei = args.args[0]
+        if ist_datei_latin1(buchungsdatei):
+            buchungsdatei = latin1_nach_utf8(buchungsdatei)
+        buchungen = buchungen_aus_datei(buchungsdatei)
+        buchungen_in_db_speichern(buchungen)
+
 
 if __name__ == '__main__':
     main()
